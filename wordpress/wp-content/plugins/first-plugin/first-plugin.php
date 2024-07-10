0c80313bc3cea94d1de84303472bd84ab3adbd39
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2024-07-10 10:32:54 +0700

    feat add first plugin

---
+++ wordpress/wp-content/plugins/first-plugin/first-plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package FirstPlugin
+ */
+/*
+Plugin Name: First Plugin
+Plugin URI: https://github.com/Quan0308/PHP
+Description: This is my first attempt on writing a custom plugin for WordPress
+Version: 1.0.0
+Author: Bui Do Duy Quan
+Author URI: https://github.com/Quan0308/PHP
+License: GPLv2 or later
+Text Domain: first-plugin
+*/
